aad03a9df53d1d0aa38ceb47721c7d7dd2d3841c
Vinícius Oliveira <vinitag190@gmail.com>
2020-05-21 10:48:26 -0300

    Aula 13

---
+++ Aula13/ambiente.js
@@ -0,0 +1,28 @@
+/*      DO WHILE
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+*/
+
+
+/*      WHILE
+var c = 1
+
+while(c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*      SEM ESTRUTURA
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+*/
