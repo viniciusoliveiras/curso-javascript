c6975e36335ac6f6661a0c4c40270e4411f08f16
Vinícius Oliveira <vinitag190@gmail.com>
2020-07-24 16:26:28 -0300

    Ex 018 (fim)

--- Aula16ex/ex016/ex018/script.js
+++ Aula16ex/ex016/ex018/script.js
@@ -3,6 +3,7 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+/* verifica se o nº está entre 1 e 100 */
 function isNumero(n){
     if(Number(n)>=1 && Number(n)<=100){
         return true
@@ -11,6 +12,7 @@ function isNumero(n){
     }
 }
 
+/* verifica se o nº já está na lista. se retornar -1 é pq ainda não está */
 function inLista(n, l){
     if(l.indexOf(Number(n))!= -1){
         return true
@@ -19,11 +21,58 @@ function inLista(n, l){
     }
 }
 
+/* adiciona os números ao vetor e na tabela */
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        valores.push(num)
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''      
     } else{
         alert("[ERROR] Número inválido ou já na lista.")
     }  
+    num.value = ""
+    num.focus()
+}
+
+/* finaliza o processo mostrando diversas infos */
+function finalizar(){
+    if(valores.length == 0){
+        alert("[ERROR] Adicione valores antes de finalizar.")
+    } else{
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    
+    for(let pos in valores){
+        soma += valores[pos]
+        if(valores[pos]>maior)
+            maior = valores[pos]
+        if(valores[pos]<menor)
+            menor = valores[pos]
+    }
+    
+    let media = soma/valores.length    
+
+    res.innerHTML = ''
+    if(valores.length == 1){
+        res.innerHTML = `<p>Ao todo, temos ${valores.length} número cadastrado.</p>`
+    } else{
+        res.innerHTML = `<p>Ao todo, temos ${valores.length} números cadastrados.</p>`
+    }
+
+    res.innerHTML += `<p>O maior número é ${maior}.</p>`
+    res.innerHTML += `<p>O menor número é ${menor}</p>`
+    res.innerHTML += `<p>A soma entre os números é ${soma}</p>`
+    res.innerHTML += `<p>A média entre os números é ${media}</p>`
+    }  
+}
+
+/* limpa todos os dados inseridos */
+function limpar(){
+    lista.innerHTML = ''
+    res.innerHTML = ''
+    valores = []
+    
 }
